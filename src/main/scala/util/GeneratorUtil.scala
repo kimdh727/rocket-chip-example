0124d1ad60b84296a02f35e603c95af22ebfdf4f
Jade Kim <kimdh_727@kakao.com>
2022-06-26 21:50:38 +0900

    Update Generator Util

--- src/main/scala/util/GeneratorUtil.scala
+++ src/main/scala/util/GeneratorUtil.scala
@@ -2,52 +2,62 @@ package rce.util
 
 import java.io.File
 
-import firrtl.stage.FirrtlStage
+import firrtl.stage._
+import firrtl.options._
+import firrtl.VerilogEmitter
+import logger.{LogLevel, LogLevelAnnotation}
+import freechips.rocketchip.stage._
 import freechips.rocketchip.system.RocketChipStage
 import freechips.rocketchip.config.Config
 
-import rce.example.config.EmptyConfig
-
 object GeneratorUtil {
+
+  private[this] def dir(path: String) = {
+    val dir = new File(path)
+    if (!dir.exists()) dir.mkdirs()
+    dir.getPath()
+  }
+
   def apply[C <: Config](
       module: Class[_],
-      configs: Seq[Class[C]] = Seq(classOf[EmptyConfig]),
-      logLevel: String = "warn"): Unit = {
+      configs: Seq[Class[C]] = Seq(classOf[rce.example.config.EmptyConfig]),
+      logLevel: String = "warn",
+  ): Unit = {
 
     val moduleName = module.getName.split("\\.").last
 
     val dirName = s"build/$moduleName"
 
-    val firrtlDirName = dirName + "/firrtl"
-    val verilogDirName = dirName + "/verilog"
+    val firrtlDir  = dir(dirName + "/firrtl")
+    val verilogDir = dir(dirName + "/verilog")
 
-    val firrtlDir = new File(firrtlDirName)
-    val verilogDir = new File(verilogDirName)
+    val commonAnnotations = Seq(
+      LogLevelAnnotation(LogLevel(logLevel))
+    )
 
-    if (!firrtlDir.exists()) firrtlDir.mkdirs()
-    if (!verilogDir.exists()) verilogDir.mkdirs()
+    val rocketChipStageAnnotations = Seq(
+      TargetDirAnnotation(firrtlDir),
+      new TopModuleAnnotation(module),
+      new ConfigsAnnotation(configs.map(_.getName())),
+      new OutputBaseNameAnnotation(moduleName)
+    )
+
+    val firrtlStageAnnotations = Seq(
+      TargetDirAnnotation(verilogDir),
+      FirrtlFileAnnotation(s"$firrtlDir/$moduleName.fir"),
+      InputAnnotationFileAnnotation(s"$firrtlDir/$moduleName.anno.json"),
+      RunFirrtlTransformAnnotation(new VerilogEmitter),
+      AllowUnrecognizedAnnotations
+    )
 
     new RocketChipStage().execute(
-      args = Array(
-        "--target-dir", firrtlDirName,
-        "--log-level", logLevel,
-        "--top-module", module.getName,
-        "--configs", configs.map(_.getName).mkString(","),
-        "--name", moduleName,
-      ),
-      annotations = Seq()
+      args = Array(),
+      annotations = commonAnnotations ++ rocketChipStageAnnotations
     )
 
     new FirrtlStage().execute(
-      args = Array(
-        "--target-dir", verilogDirName,
-        "--log-level", logLevel,
-        "--input-file", s"$firrtlDirName/$moduleName.fir",
-        "--emit-modules", "verilog",
-        "--annotation-file", s"$firrtlDirName/$moduleName.anno.json",
-        "--allow-unrecognized-annotations",
-      ),
-      annotations = Seq()
+      args = Array(),
+      annotations = commonAnnotations ++ firrtlStageAnnotations
     )
   }
 }
